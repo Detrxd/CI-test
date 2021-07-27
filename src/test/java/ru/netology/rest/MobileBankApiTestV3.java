01969aca0534aea0c45e90390cec1b3706eb73dd
Ilya Filimonov <ilyafil1996@yandex.ru>
2021-07-27 11:39:28 +0300

    Adding schema & rework API Test V#

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -5,6 +5,7 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV3 {
@@ -20,8 +21,8 @@ class MobileBankApiTestV3 {
       // Проверки
       .then()
           .statusCode(200)
-          // специализированные проверки - лучше
-          .contentType(ContentType.JSON)
+          // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
           .body("", hasSize(3))
           .body("[0].currency", equalTo("RUB"))
           .body("[0].balance", greaterThanOrEqualTo(0))
