b2efc5c82e772a8654aa313666d25514e1e8b8bf
Ilya Filimonov <ilyafil1996@yandex.ru>
2021-07-28 11:06:56 +0300

    Rework schema for 'RUB|USD'

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -11,21 +11,21 @@ import static org.hamcrest.Matchers.*;
 class MobileBankApiTestV3 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200)
-          // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
-          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-          .body("", hasSize(3))
-          .body("[0].currency", equalTo("USD"))
-          .body("[0].balance", greaterThanOrEqualTo(0))
-      ;
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+                .body("", hasSize(3))
+                .body("[0].currency", equalTo("RUB"))
+                .body("[0].balance", greaterThanOrEqualTo(0))
+        ;
     }
 }
